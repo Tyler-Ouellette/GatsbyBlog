5fe618097cdc18d4ddb88c17148be12bd76dd6bf
Tyler <ouell117@uwindsor.ca>
2020-01-24 17:34:51 -0500

    icon and pages created

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,33 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+import gatsbyLogo from '../images/gatsby-icon.png'
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
       background: `rebeccapurple`,
       marginBottom: `1.45rem`,
-    }}
-  >
+    }}>
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
-      }}
-    >
+      }}>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
             color: `white`,
             textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+          }}>
+          <img
+            style={{
+              width: '100px',
+            }}
+            src={gatsbyLogo}
+            alt=""></img>
         </Link>
       </h1>
     </div>
---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas sunt illum
+        eligendi, exercitationem repellendus minus hic veritatis laborum
+        quibusdam nisi cum veniam iure aut rem provident? Praesentium in
+        inventore sapiente.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default About
